511661f28157466ff79ff183fb8a2482ba881e4c
jake waters <jerkey@g-cipher.net>
2017-05-10 22:40:09 -0700

    strip out unused gasoline legacy code

--- Canbus.cpp
+++ Canbus.cpp
@@ -18,9 +18,7 @@
 #include <util/delay.h>
 #include "pins_arduino.h"
 #include <inttypes.h>
-#include "global.h"
 #include "mcp2515.h"
-#include "defaults.h"
 #include "Canbus.h"
 
 
@@ -74,95 +72,19 @@ char CanbusClass::message_tx(void) {
   message.data[6] = 0x00;
   message.data[7] = 0x00;
 
-
-
-
-//  mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), (1<<REQOP1));
-    mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
-
   if (mcp2515_send_message(&message)) {
-    //  SET(LED2_HIGH);
     return 1;
-
-  }
-  else {
-  //  PRINT("Fehler: konnte die Nachricht nicht auslesen\n\n");
-  return 0;
+  } else {
+    Serial.println("Error: could not read the message");
+    return 0;
   }
 return 1;
-
 }
-
-char CanbusClass::ecu_req(unsigned char pid,  char *buffer)
-{
-  tCAN message;
-  float engine_data;
-  int timeout = 0;
-  char message_ok = 0;
-  // Prepair message
-  message.id = PID_REQUEST;
-  message.header.rtr = 0;
-  message.header.length = 8;
-  message.data[0] = 0x02;
-  message.data[1] = 0x01;
-  message.data[2] = pid;
-  message.data[3] = 0x00;
-  message.data[4] = 0x00;
-  message.data[5] = 0x00;
-  message.data[6] = 0x00;
-  message.data[7] = 0x00;
-
-
-  mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
-
-  if (mcp2515_send_message(&message)) { }
-  if (mcp2515_check_message()) {
-    if (mcp2515_get_message(&message)) {
-        switch(message.data[2])
-          {   /* Details from http://en.wikipedia.org/wiki/OBD-II_PIDs */
-            case ENGINE_RPM:        //   ((A*256)+B)/4    [RPM]
-            engine_data =  ((message.data[3]*256) + message.data[4])/4;
-            sprintf(buffer,"%d rpm ",(int) engine_data);
-            break;
-
-            case ENGINE_COOLANT_TEMP:   //   A-40        [degree C]
-            engine_data =  message.data[3] - 40;
-            sprintf(buffer,"%d degC",(int) engine_data);
-            break;
-
-            case VEHICLE_SPEED:     // A          [km]
-            engine_data =  message.data[3];
-            sprintf(buffer,"%d km ",(int) engine_data);
-            break;
-
-            case MAF_SENSOR:         // ((256*A)+B) / 100  [g/s]
-            engine_data =  ((message.data[3]*256) + message.data[4])/100;
-            sprintf(buffer,"%d g/s",(int) engine_data);
-            break;
-
-            case O2_VOLTAGE:        // A * 0.005   (B-128) * 100/128 (if B==0xFF, sensor is not used in trim calc)
-            engine_data = message.data[3]*0.005;
-            sprintf(buffer,"%d V",(int) engine_data);
-            break;
-
-            case THROTTLE:        // Throttle Position
-            engine_data = (message.data[3]*100)/255;
-            sprintf(buffer,"%d %% ",(int) engine_data);
-            break;
-
-          }
-    }
-  }
-
-}
-
-
-
-
 
 
 char CanbusClass::init(unsigned char speed) {
 
+  //mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0); // i guess this tells the chip to receive everything?
   return mcp2515_init(speed);
 
 }
--- Canbus.h
+++ Canbus.h
@@ -11,26 +11,14 @@
 #define CANSPEED_250  	3		// CAN speed at 250 kbps
 #define CANSPEED_500	1		// CAN speed at 500 kbps
 
-
-#define ENGINE_COOLANT_TEMP 0x05
-#define ENGINE_RPM          0x0C
-#define VEHICLE_SPEED       0x0D
-#define MAF_SENSOR          0x10
-#define O2_VOLTAGE          0x14
-#define THROTTLE			0x11
-
-#define PID_REQUEST         0x7DF
-#define PID_REPLY			0x7E8
-
 class CanbusClass
 {
   public:
 
 	CanbusClass();
-    char init(unsigned char);
+        char init(unsigned char);
 	char message_tx(void);
 	char message_rx(unsigned char *buffer, uint16_t *id, uint8_t *length);
-	char ecu_req(unsigned char pid,  char *buffer);
 private:
 
 };
