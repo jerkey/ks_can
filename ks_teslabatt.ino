5501ceb1397c0aaf40ba17466062c040b7f70566
jake waters <jerkey@g-cipher.net>
2018-03-04 18:43:52 -0800

    pretend to be a happy tesla battery

--- ks_teslabatt.ino
+++ ks_teslabatt.ino
@@ -2,24 +2,16 @@
 // E:F5, H:DA, L:BF  avrdude -p m1280 -U lfuse:w:0xBF:m
 #include "Canbus.h"
 #include "mcp2515.h"
-#include "zero.h"
 byte buffer[16];
-uint16_t command;
-uint8_t num_modules;
-uint16_t zero_control_tail = 0; // last two bytes of Canbus.zero_control() message
-uint16_t cellVoltages[32];
-uint32_t packVoltage = 0;
 uint16_t id;
 uint8_t length;
-uint16_t x188count=0;
-uint16_t x408count=0;
-uint16_t x308count=0;
-unsigned long lastCellPrint = 0; // last time we printed cell voltages
+uint16_t x126count=0;
+uint16_t DI_vBat; // from BO_ 294
+uint16_t DI_motorCurrent = 0; // from BO_ 294
 
 void setup(){
 Serial.begin(230400);
 Serial.println("CAN-Bus Demo");
-for (int i=0; i<32; i++) cellVoltages[i] = 0; // zero battery voltages
 
 if(Canbus.init(CANSPEED_500)) {
     Serial.println("CAN Init ok");
@@ -29,90 +21,74 @@ if(Canbus.init(CANSPEED_500)) {
 }
 
 void loop(){
-  send506(); // tell the battery what we want
+  fakeBMS();
   id=0;
   Canbus.message_rx(buffer,&id,&length);
-  if (id == 0x0188) { if (x188count++ > 10) { x188count=0; // add ++ after first x???count to enable display
-      Serial.print("SOC:");
-      Serial.print(buffer[0]);
-      Serial.print("  status:");
-      printBMSStatusMessages(buffer[1]+buffer[2]*256);
-      Serial.print("  charge cycles:");
-      Serial.print(buffer[4]*256+buffer[3]);
-      Serial.print("  balance mV:");
-      Serial.print(buffer[6]*256+buffer[5]);
-      Serial.print("  number of bricks:");
-      Serial.print(buffer[7]);
-      Serial.print("  command: 0x");
-      Serial.print(command, HEX);
-      Serial.print("  num_modules: ");
-      Serial.print(num_modules);
-      Serial.print("  time: ");
-      Serial.println((float)millis() / 1000.0,2);
-    }
-  } else if (id == 0x0408) { if (x408count > 8) { x408count=0; // add ++ after first x???count to enable display
-      Serial.print("Highest FET temp C:");
-      Serial.print(buffer[0]);
-      Serial.print("  Highest Pack Temp C:");
-      Serial.print(buffer[1]);
-      Serial.print("  Lowest Pack Temp C:");
-      Serial.print(buffer[2]);
-      Serial.print("  Pack Discharge Current Amps:");
-      Serial.print(buffer[4]*256+buffer[3]);
-      Serial.print("  Pack Capacity Remaining AH:");
-      Serial.println(buffer[6]*256+buffer[5]);
-    }
-  } else if (id == 0x388 && buffer[0]<32) { // cell voltages won't fully populate unless 0x506 traffic is happening on the canbus
-    cellVoltages[buffer[0]] = (buffer[2] << 8) + buffer[1];
-    packVoltage = ((uint32_t)buffer[5] << 16) + ((uint16_t)buffer[4] << 8) + buffer[3];
-    if (millis() - lastCellPrint > 60000) { // how much time between printing cell voltages
-      lastCellPrint = millis();
-      Serial.print("cell voltages: ");
-      for (int i=0; i<28; i++) {
-        if (cellVoltages[i]>0) {
-        Serial.print((float)cellVoltages[i]/1000,2);
-        Serial.print(" ");
-        } else {
-          Serial.print("-.-- ");
-        }
-      }
-      Serial.print("  Pack voltage: ");
-      Serial.println((float)packVoltage/1000,2);
-    }
-  } else if (id == 0x488) {// Serial.print("488 ");printBuf();
-  } else if (id == 0x508) {// Serial.print("508 ");printBuf();
-  } else if (id == 0x308) { if (x308count > 4) { x308count=0;
-      Serial.print("BMS firmware Revision: ");
-      Serial.print(buffer[0]);
-      Serial.print("  BMS Board Revision: ");
-      Serial.print(buffer[1]);
-      Serial.print("  Run Time Seconds:");
-      Serial.print(buffer[3]*256+buffer[2]);
-      Serial.print("  Total Energy Used WH:");
-      Serial.println(buffer[4]+buffer[5]*256+buffer[6]*65536+buffer[7]*16777216);
-    }
+  if (id == 0x0126) { if (x126count++ > 5) { x126count=0; // add ++ after first x???count to enable display
+    // BO_ 294 DI_motorControl: 8 DI  SG_ DI_vBat : 0|10@1+ (0.5,0) [0|500] "V" GTW  SG_ DI_motorCurrent : 16|11@1+ (1,0) [0|2047] "A" GTW
+    DI_vBat = (((uint16_t)buffer[0]) + (((uint16_t)buffer[1] & 3) << 8)) / 2;
+    DI_motorCurrent = (((uint16_t)buffer[2]) + ((uint16_t)(buffer[3] & 7) << 8));
+    Serial.print("DI_vBat:");
+    Serial.print(DI_vBat);
+    Serial.print("   DI_motorCurrent:");
+    Serial.println(DI_motorCurrent);
+  }
   } else if (id == 0x288) {// Serial.print("288 ");printBuf();
-  } else if (id) {
-    Serial.print(" 0x");
-    Serial.print(id,HEX);
+  } else if (id) {// Serial.print(" 0x"); Serial.print(id,HEX);
   }
 }
 
-void send506() {
-  command   = CODE_NOT_SAFETY_OVERRIDE + CODE_DISCONNECT_MODULE + CODE_OPEN_CONTACTOR; // charger 1
-  //command = CODE_NOT_SAFETY_OVERRIDE + CODE_KEY_ON + CODE_DISCONNECT_MODULE + CODE_OPEN_CONTACTOR + CODE_OPEN_FET; // first CAN 1
-  num_modules = 0;
-  //zero_control_tail = 0xFFFF; // send FF's for last two bytes (not when charging though?)
-  if (millis() > 5000) {
-    command += CODE_CHARGER_CONNECTED; // charger 2
-    //command = CODE_NOT_SAFETY_OVERRIDE + CODE_KEY_ON + CODE_CONNECT_MODULE  + CODE_OPEN_CONTACTOR + CODE_CLOSE_FET; // first CAN 2
+void fakeBMS() {
+  unsigned long time = millis();
+
+  static unsigned long every10 = 0;
+  if (time - every10 > 10) {
+    every10 = time; // reset interval timer
+    id=0x102; // every 10ms   // 0x102 - Dec 258 - BMS - HV Bus Status
+    length=8;                 // 0x16 0x92 0x03 0xA7 0x06 0x4E 0x43 0x01
+    buffer[0]=DI_vBat*100%256;
+    buffer[1]=DI_vBat*100/256;
+    buffer[2]=0x03;
+    buffer[3]=0xA7;
+    buffer[4]=0x06;
+    buffer[5]=0x4E;
+    buffer[6]=0x43;
+    buffer[7]=0x01;
+    Canbus.message_tx(buffer,id,length);
   }
-  if (millis() > 10000) {
-    num_modules = 1;
-    //command = CODE_NOT_SAFETY_OVERRIDE + CODE_KEY_ON + CODE_CONNECT_MODULE  + CODE_CLOSE_CONTACTOR + CODE_CLOSE_FET; // first CAN 3
-    command = CODE_NOT_SAFETY_OVERRIDE + CODE_CHARGING_ENABLED + CODE_CHARGER_CONNECTED + CODE_CONNECT_MODULE + CODE_OPEN_CONTACTOR + CODE_CLOSE_CONTACTOR; // charger 3
+
+  static unsigned long every100 = 0;
+  if (time - every100 > 100) {
+    every100 = time; // reset interval timer
+    id=0x202; // every 100ms      // 0x202 - Dec 514 - BMS - Drive Limits
+    length=8;
+    buffer[0]=0xF7;
+    buffer[1]=0x5E;
+    buffer[2]=0x4E;
+    buffer[3]=0x9D;
+    buffer[4]=0xC4;
+    buffer[5]=0x09;
+    buffer[6]=0x3F;
+    buffer[7]=0x23;
+    Canbus.message_tx(buffer,id,length);
+
+    id=0x212; // every 100ms      // 0x212 - Dec 530 - BMS - Status
+    length=5;
+    buffer[0]=0xD8;
+    buffer[1]=0x08;
+    buffer[2]=0x12;
+    buffer[3]=0x72;
+    buffer[4]=0x00;
+    Canbus.message_tx(buffer,id,length);
+
+    id=0x232; // every 100ms      // 0x232 - Dec 562 - BMS - Power Available (Max Regen and Discharge)
+    length=4;
+    buffer[0]=0x1B;           // 16 bit Max Regen power in kW (divide 16 bit by 100, so 65535 = 655.35kW)
+    buffer[1]=0x19;           // 191B = 64.27kW  (26EA = 99.62kW)
+    buffer[2]=0xF5;           // 16 bit Max Drive power in kW (divide 16 bit by 100, so 65535 = 655.35kW)
+    buffer[3]=0x84;           // 84F5 = 340.37kW  (7EA3 = 324.19)
+    Canbus.message_tx(buffer,id,length);
   }
-  Canbus.zero_control(8,command,num_modules,zero_control_tail);
 }
 
 void printBuf() {
